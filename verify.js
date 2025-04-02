f5629eb5f26401436d13b2a52c70f1451ae9c678
Marco <maidh91@gmail.com>
2025-04-02 20:55:52 +1100

    move logs

--- verify.js
+++ verify.js
@@ -1,14 +1,32 @@
 #!/usr/bin/env node
 
+const z32 = require('z32')
+
+const { version } = require('./package.json')
 const verify = require('./lib/verify')
 
 async function main () {
   const response = process.argv[2]
   const signingRequest = process.argv[3]
   const pubkey = process.argv[4]
+  if (!response || !signingRequest || !pubkey) {
+    console.log(`hypercore-verify ${version}\n`)
+    console.log('Verify a signed message.')
+    console.log('\nUsage:')
+    console.log('hypercore-verify <response> <signingRequest> <pubkey>\n')
+    process.exit(1)
+  }
 
   try {
-    await verify(response, signingRequest, pubkey)
+    const req = await verify(response, signingRequest, pubkey)
+    console.log('\nSignature verified.')
+    console.log(`\n${pubkey} has signed the following request:`)
+    console.log({
+      core: req.id,
+      fork: req.fork,
+      length: req.length,
+      treeHash: req.treeHash.toString('hex')
+    })
   } catch (err) {
     console.error(err)
     process.exit(1)
