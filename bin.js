6b9cf0db331b1964c64add468c970db11d422b10
Marco <maidh91@gmail.com>
2026-02-09 20:56:43 +1100

    deprecated cli

---
+++ bin.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+console.log('This binary is deprecated. Use hypercore-sign-cli instead!');
+console.log(`
+npm uninstall -g hypercore-sign
+npm install -g hypercore-sign-cli
+`)
