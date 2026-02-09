cb2c8988f3190d0f497832abaf00d07699b8dc7e
Marco <maidh91@gmail.com>
2026-02-09 20:26:58 +1100

    extract-hypercore-sign-cli

--- index.js
+++ index.js
@@ -1,4 +1,265 @@
+const c = require('compact-encoding')
+const crypto = require('hypercore-crypto')
+const request = require('hypercore-signing-request')
+const sodium = require('sodium-native')
+const z32 = require('z32')
+
+const MAX_SUPPORTED_VERSION = 2
+
+const labelledKey = {
+  preencode(state, k) {
+    c.fixed(8).preencode(state, k.id)
+    c.fixed64.preencode(state, k.secretKey)
+  },
+  encode(state, k) {
+    c.fixed(8).encode(state, k.id)
+    c.fixed64.encode(state, k.secretKey)
+  },
+  decode(state) {
+    throw new Error('No decoder')
+  }
+}
+
+const keyDescriptor = {
+  preencode(state, s) {
+    c.fixed(8).preencode(state, s.id)
+    c.fixed64.preencode(state, s.secretKey)
+    c.fixed32.preencode(state, s.checkSum)
+  },
+  encode(state, s) {
+    c.fixed(8).encode(state, s.id)
+    c.fixed64.encode(state, s.secretKey)
+    c.fixed32.encode(state, s.checkSum)
+  },
+  decode(state) {
+    return {
+      id: c.fixed(8).decode(state),
+      secretKey: c.fixed64.decode(state),
+      checkSum: c.fixed32.decode(state)
+    }
+  }
+}
+
+const kdfParams = {
+  preencode(state, p) {
+    c.uint64.preencode(state, p.ops)
+    c.uint64.preencode(state, p.mem)
+  },
+  encode(state, p) {
+    c.uint64.encode(state, p.ops)
+    c.uint64.encode(state, p.mem)
+  },
+  decode(state) {
+    return {
+      ops: c.uint64.decode(state),
+      mem: c.uint64.decode(state)
+    }
+  }
+}
+
+const encryptedKey = {
+  preencode(state, s) {
+    kdfParams.preencode(state, s.params)
+    c.fixed32.preencode(state, s.salt)
+    c.buffer.preencode(state, s.payload)
+  },
+  encode(state, s) {
+    kdfParams.encode(state, s.params)
+    c.fixed32.encode(state, s.salt)
+    c.buffer.encode(state, s.payload)
+  },
+  decode(state) {
+    return {
+      params: kdfParams.decode(state),
+      salt: c.fixed32.decode(state),
+      payload: c.buffer.decode(state)
+    }
+  }
+}
+
+const signatures = c.array(c.fixed64)
+
+const Response = {
+  preencode(state, res) {
+    c.uint.preencode(state, res.version)
+    c.fixed32.preencode(state, res.requestHash)
+    c.fixed32.preencode(state, res.publicKey)
+    signatures.preencode(state, res.signatures)
+  },
+  encode(state, res) {
+    c.uint.encode(state, res.version)
+    c.fixed32.encode(state, res.requestHash)
+    c.fixed32.encode(state, res.publicKey)
+    signatures.encode(state, res.signatures)
+  },
+  decode(state, res) {
+    const version = c.uint.decode(state)
+
+    if (version > MAX_SUPPORTED_VERSION) {
+      throw new Error('Response version is not supported, please upgrade')
+    }
+
+    return {
+      version,
+      requestHash: c.fixed32.decode(state),
+      publicKey: c.fixed32.decode(state),
+      signatures: signatures.decode(state)
+    }
+  }
+}
+
+function generateKeys(pwd) {
+  const id = Buffer.alloc(8)
+
+  const publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
+  const secretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES)
+
+  const salt = Buffer.alloc(32)
+  const kdfOutput = Buffer.alloc(8 + 64 + 32)
+  const checkSum = Buffer.alloc(sodium.crypto_generichash_BYTES)
+
+  const params = {
+    ops: sodium.crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_SENSITIVE,
+    mem: sodium.crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_SENSITIVE
+  }
+
+  sodium.randombytes_buf(id)
+  sodium.randombytes_buf(salt)
+
+  sodium.crypto_sign_keypair(publicKey, secretKey)
+
+  const checkSumData = c.encode(labelledKey, { id, secretKey })
+
+  sodium.crypto_generichash(checkSum, checkSumData)
+  sodium.sodium_memzero(checkSumData)
+
+  const payload = c.encode(keyDescriptor, {
+    id,
+    secretKey,
+    checkSum
+  })
+
+  sodium.sodium_memzero(secretKey)
+
+  sodium.sodium_mprotect_readwrite(pwd)
+  sodium.crypto_pwhash_scryptsalsa208sha256(kdfOutput, pwd, salt, params.ops, params.mem)
+  sodium.sodium_memzero(pwd)
+  sodium.sodium_free(pwd)
+
+  xor(payload, kdfOutput)
+  sodium.sodium_memzero(kdfOutput)
+
+  const encrypted = c.encode(encryptedKey, {
+    params,
+    salt,
+    payload
+  })
+
+  sodium.sodium_memzero(payload)
+
+  return {
+    id,
+    publicKey,
+    secretKey: encrypted
+  }
+}
+
+function sign(signables, keyBuffer, pwd) {
+  const { params, salt, payload } = c.decode(encryptedKey, keyBuffer)
+
+  const kdfOutput = Buffer.alloc(8 + 64 + 32)
+
+  sodium.sodium_mprotect_readwrite(pwd)
+  sodium.crypto_pwhash_scryptsalsa208sha256(kdfOutput, pwd, salt, params.ops, params.mem)
+  sodium.sodium_memzero(pwd)
+  sodium.sodium_free(pwd)
+
+  xor(payload, kdfOutput)
+  sodium.sodium_memzero(kdfOutput)
+
+  const { id, secretKey, checkSum } = c.decode(keyDescriptor, payload)
+
+  const checkAgainst = Buffer.alloc(sodium.crypto_generichash_BYTES)
+  const checkSumData = c.encode(labelledKey, { id, secretKey })
+
+  sodium.crypto_generichash(checkAgainst, checkSumData)
+  sodium.sodium_memzero(checkSumData)
+
+  if (Buffer.compare(checkAgainst, checkSum) !== 0) {
+    sodium.sodium_memzero(secretKey)
+    sodium.sodium_memzero(payload)
+    throw new Error('Key decryption failed')
+  }
+
+  const signatures = []
+  for (const { signable } of signables) {
+    const signature = Buffer.alloc(sodium.crypto_sign_BYTES)
+    sodium.crypto_sign_detached(signature, signable, secretKey)
+    signatures.push(signature)
+  }
+
+  sodium.sodium_memzero(secretKey)
+  sodium.sodium_memzero(payload)
+
+  return signatures
+}
+
+function xor(a, b) {
+  if (a.byteLength !== b.byteLength) {
+    throw new Error('Buffers should be equal in size')
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    a[i] ^= b[i]
+  }
+}
+
+function verify(response, signingRequest, pubkey) {
+  const res = c.decode(Response, z32.decode(response))
+  const publicKey = z32.decode(pubkey)
+
+  if (!response || !signingRequest || !pubkey) {
+    throw new Error('Invalid arguments')
+  }
+
+  let req = null
+  try {
+    req = request.decode(z32.decode(signingRequest))
+  } catch (e) {
+    throw new Error('Invalid signing request')
+  }
+
+  if (req.version > MAX_SUPPORTED_VERSION) {
+    throw new Error('Request version not supported, please update')
+  }
+
+  if (Buffer.compare(res.requestHash, crypto.hash(z32.decode(signingRequest))) !== 0) {
+    throw new Error('Signature was not made over this request')
+  }
+
+  if (Buffer.compare(res.publicKey, publicKey) !== 0) {
+    throw new Error('Public key does not match')
+  }
+
+  const signables = request.signable(publicKey, req)
+
+  if (signables.length !== res.signatures.length) {
+    throw new Error('Invalid response: signature count does not match')
+  }
+
+  for (let i = 0; i < signables.length; i++) {
+    if (!sodium.crypto_sign_verify_detached(res.signatures[i], signables[i].signable, publicKey)) {
+      throw new Error('Invalid signature!')
+    }
+  }
+
+  return req
+}
+
 module.exports = {
-  messages: require('./lib/messages'),
-  verify: require('./lib/verify')
+  MAX_SUPPORTED_VERSION,
+  Response,
+  generateKeys,
+  sign,
+  verify
 }
---
+++ test.js
@@ -0,0 +1,148 @@
+const b4a = require('b4a')
+const test = require('brittle')
+const cenc = require('compact-encoding')
+const Corestore = require('corestore')
+const crypto = require('hypercore-crypto')
+const CoreRequest = require('hypercore-signing-request')
+const createTestnet = require('hyperdht/testnet')
+const Hyperdrive = require('hyperdrive')
+const Hyperswarm = require('hyperswarm')
+const sodium = require('sodium-native')
+const z32 = require('z32')
+
+const CoreSign = require('.')
+
+test('generateKeys', async (t) => {
+  const [keys] = setupSigners()
+  t.ok(keys.id, 'should have id')
+  t.ok(keys.publicKey, 'should have publicKey')
+  t.ok(keys.secretKey, 'should have secretKey')
+})
+
+test('sign a core and verify', async (t) => {
+  const signers = setupSigners()
+  const { store } = await setupReplication(t)
+
+  const request = await getSigningRequest(signers, store)
+  const response = sign(request, signers[0])
+  t.ok(response, 'should have response')
+  CoreSign.verify(response, z32.encode(request), z32.encode(signers[0].publicKey))
+  t.pass('should verify signing request')
+})
+
+test('sign a drive and verify', async (t) => {
+  const signers = setupSigners()
+  const { store } = await setupReplication(t)
+
+  const request = await getDriveSigningRequest(signers, store)
+  const response = sign(request, signers[0])
+  t.ok(response, 'should have response')
+  CoreSign.verify(response, z32.encode(request), z32.encode(signers[0].publicKey))
+  t.pass('should verify drive signing request')
+})
+
+function sign(request, signer) {
+  // clone to avoid mutation
+  const clonedSigner = Object.keys(signer).reduce((acc, key) => {
+    acc[key] = b4a.from(signer[key])
+    return acc
+  }, {})
+
+  const decodedReq = CoreRequest.decode(request)
+  const signables = CoreRequest.signable(clonedSigner.publicKey, decodedReq)
+
+  const password = sodium.sodium_malloc(8)
+  sodium.randombytes_buf_deterministic(password, clonedSigner.seed)
+
+  const signatures = CoreSign.sign(signables, clonedSigner.secretKey, password)
+
+  const res = cenc.encode(CoreSign.Response, {
+    version: decodedReq.version,
+    requestHash: crypto.hash(request),
+    publicKey: clonedSigner.publicKey,
+    signatures
+  })
+  return z32.encode(res)
+}
+
+async function getSigningRequest(signers, store) {
+  const core = store.get({ name: 'test' })
+  await core.ready()
+  await core.append('Block 0')
+  await core.append('Block 1')
+
+  const namespace = b4a.alloc(32, 1)
+  const manifest = {
+    version: 1,
+    hash: 'blake2b',
+    quorum: 1,
+    signers: signers.map(({ publicKey }) => ({
+      signature: 'ed25519',
+      publicKey,
+      namespace
+    }))
+  }
+  return CoreRequest.generate(core, { manifest })
+}
+
+async function getDriveSigningRequest(signers, store) {
+  const drive = new Hyperdrive(store)
+  await drive.ready()
+  await drive.put('a', b4a.from('Block 0'))
+  await drive.put('b', b4a.from('Block 1'))
+
+  const manifest = {
+    version: 1,
+    hash: 'blake2b',
+    quorum: 1,
+    signers: signers.map(({ publicKey }) => ({
+      signature: 'ed25519',
+      publicKey,
+      namespace: b4a.alloc(32, 1)
+    }))
+  }
+  return CoreRequest.generateDrive(drive, { manifest })
+}
+
+function setupSigners(n = 1) {
+  const signers = []
+  for (let i = 0; i < n; i++) {
+    const seed = sodium.sodium_malloc(sodium.randombytes_SEEDBYTES)
+    sodium.randombytes_buf(seed)
+    const password = sodium.sodium_malloc(8)
+    sodium.randombytes_buf_deterministic(password, seed)
+
+    const keys = CoreSign.generateKeys(password)
+    signers.push({ ...keys, seed })
+  }
+  return signers
+}
+
+async function setupReplication(t, n = 1, network) {
+  const res = network ?? (await setupTestnet(t))
+  const { bootstrap } = res
+
+  for (let step = 1; step <= n; step++) {
+    const storage = await t.tmp()
+    const store = new Corestore(storage)
+    t.teardown(() => store.close(), { order: 4000 })
+    const swarm = new Hyperswarm({ bootstrap })
+    t.teardown(() => swarm.destroy(), { order: 3000 })
+
+    swarm.on('connection', (conn) => store.replicate(conn))
+
+    const nstring = step > 1 ? step : ''
+    res[`storage${nstring}`] = storage
+    res[`store${nstring}`] = store
+    res[`swarm${nstring}`] = swarm
+  }
+
+  return res
+}
+
+async function setupTestnet(t) {
+  const testnet = await createTestnet()
+  t.teardown(() => testnet.destroy(), { order: 5000 })
+  const bootstrap = testnet.bootstrap
+  return { testnet, bootstrap }
+}
