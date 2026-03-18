326347d742a58e619a1ea7a5ce94c00832e66e3a
Marco <maidh91@gmail.com>
2026-03-19 07:10:39 +1100

    extract hypercore-sign-cli (#20)
    * extract-hypercore-sign-cli
    
    * readme
    
    * hypercore-sign
    
    * ci
    
    * deprecated cli
    
    * lint
    
    * revert version
    
    * log error and exit 1
    
    * readme
    
    * process.exit
    
    * compact-encoding prod
    
    * request is accepted directly
    
    * fix package.json and tests
    
    * review by @HDegroote
    
    ---------
    
    Co-authored-by: Christophe Diederichs <chm-diederichs@protonmail.com>

---
+++ bin.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+console.error('This binary is deprecated. Use hypercore-sign-cli instead!')
+console.error(`
+npm uninstall -g hypercore-sign
+npm install -g hypercore-sign-cli
+`)
+process.exit(1)
