af6b85fefa97018ed412fc5f201f41c5af8675ba
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2026-03-31 11:19:25 +0100

    Handle legacy keys (#25)
    * check keyBuffer length for compat key encoding
    
    * unlock buffer before zeroing to prevent segfault

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const request = require('hypercore-signing-request')
 const sodium = require('sodium-native')
 
 const { LabelledKey, KeyDescriptor, EncryptedKey } = require('./lib/encoding.js')
-const { MAX_KEY_VERSION } = require('./lib/constants.js')
+const { COMPAT_VERSION, MAX_KEY_VERSION } = require('./lib/constants.js')
 
 function generateKeys(pwd) {
   const id = Buffer.alloc(8)
@@ -70,7 +70,7 @@ function generateKeys(pwd) {
   }
 }
 
-function sign(signingRequest, keyBuffer, pwd) {
+function sign(signingRequest, keyBuffer, pwd, publicKey = null) {
   let req = null
   try {
     req = request.decode(signingRequest)
@@ -90,6 +90,9 @@ function sign(signingRequest, keyBuffer, pwd) {
   }
 
   const { version, params, salt, payload, publicKey } = key
+
+  // version >= 1 has public key stored inline
+  if (version > COMPAT_VERSION) publicKey = key.publicKey
 
   if (version > MAX_KEY_VERSION) {
     free(pwd)
@@ -179,6 +182,7 @@ function verify(response, signingRequest, publicKey) {
 }
 
 function free(buffer) {
+  if (buffer.secure) sodium.sodium_mprotect_readwrite(buffer)
   sodium.sodium_memzero(buffer)
   sodium.sodium_free(buffer)
 }
