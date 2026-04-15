445c59f524ed0c1349e10e8baa29a4f2ca1aa93b
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2026-04-15 12:53:10 +0100

    use hypercore-sign-lib (#26)
    * use hypercore-sign-lib
    
    Co-authored-b: Marco <maidh91@gmail.com>
    
    * add migration pathway
    
    * handle failed migration
    
    * review by @HDegroote
    
    * lint
    
    * use b4a
    
    * add key dir log

---
+++ generate-keys.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+const fsProm = fs.promises
+const { generateKeys } = require('hypercore-sign-lib')
+const os = require('os')
+const z32 = require('z32')
+
+const homeDir = os.homedir()
+
+const { readPassword, confirmPassword } = require('./lib/password')
+const { USER_ONLY_R, USER_ONLY_RW, USER_ONLY_RWX } = require('./lib/permissions')
+
+async function main() {
+  const dir = process.env.HYPERCORE_SIGN_KEYS_DIRECTORY || path.join(homeDir, '.hypercore-sign')
+
+  await fsProm.mkdir(dir, { mode: USER_ONLY_RWX, recursive: true })
+  const secretKeyPath = path.join(dir, 'default')
+  const publicKeyPath = path.join(dir, 'default.public')
+
+  if (fs.existsSync(secretKeyPath)) {
+    console.log(`Secret key already written to ${secretKeyPath}`)
+    console.log(`Public key already written to ${publicKeyPath}`)
+    console.log()
+    console.log('Public key is', fs.readFileSync(publicKeyPath, 'utf8'))
+    return
+  }
+
+  console.log('Your secret key will be encrypted with a password.')
+  console.log('Please choose one now:\n')
+  const password = await readPassword()
+
+  if (!(await confirmPassword(password))) {
+    console.log('Passwords do not match')
+    process.exit(1)
+  }
+
+  const { secretKey, publicKey } = await generateKeys(password)
+
+  // Prompt a confirmation when overwriting
+  // (Because you probably don't want to overwrite these,
+  // once they have been generated)
+
+  await fsProm.writeFile(secretKeyPath, z32.encode(secretKey), {
+    mode: USER_ONLY_R
+  })
+
+  await fsProm.writeFile(publicKeyPath, z32.encode(publicKey), {
+    mode: USER_ONLY_RW
+  })
+
+  console.log(`\nSecret key written to ${secretKeyPath}`)
+  console.log(`Public key written to ${publicKeyPath}`)
+  console.log()
+  console.log('Public key is', z32.encode(publicKey))
+}
+
+main()
---
+++ test/e2e.js
@@ -0,0 +1,600 @@
+const fs = require('fs/promises')
+const path = require('path')
+const test = require('brittle')
+const tmpDir = require('test-tmp')
+const Hypercore = require('hypercore')
+const crypto = require('hypercore-crypto')
+const Hyperdrive = require('hyperdrive')
+const Corestore = require('corestore')
+const { getKeyInfo } = require('hypercore-sign-lib')
+const { generate, decodeResponse } = require('hypercore-signing-request')
+const { spawn } = require('child_process')
+const b4a = require('b4a')
+const z32 = require('z32')
+
+const DEBUG_LOG = false
+const DUMMY_PASSWORD = Math.random().toString().slice(2).padStart(8, 'x')
+
+test('e2e - sign a core', async (t) => {
+  const keysDir = await t.tmp()
+
+  const tCreateKeys = t.test()
+  tCreateKeys.plan(2)
+
+  const env = {
+    ...process.env,
+    HYPERCORE_SIGN_KEYS_DIRECTORY: keysDir
+  }
+
+  const genKeysProcess = spawn('node', ['generate-keys.js'], { env })
+  genKeysProcess.on('close', (code) => {
+    tCreateKeys.is(code, 0, 'Successfully created keys')
+  })
+
+  let publicKey = null
+  try {
+    genKeysProcess.stdout.on('data', (bufferData) => {
+      const data = bufferData.toString().toLowerCase()
+
+      if (DEBUG_LOG) console.log('[generate-keys]', data.toString())
+
+      if (data.includes('password:')) {
+        // Enter the password
+        genKeysProcess.stdin.write(DUMMY_PASSWORD)
+      }
+      if (data.includes('public key is')) {
+        tCreateKeys.pass('Key creation done')
+        publicKey = data.split('public key is ')[1].trim()
+      }
+    })
+
+    genKeysProcess.stderr.on('data', (data) => {
+      console.error(data.toString())
+      t.fail('generate-keys errored')
+    })
+
+    await tCreateKeys
+  } finally {
+    // To ensure the process is always killed
+    genKeysProcess.kill('SIGKILL')
+  }
+
+  const readPublicKey = await fs.readFile(path.join(keysDir, 'default.public'), 'utf-8')
+  t.alike(publicKey, readPublicKey, 'Public key got written to file')
+
+  const { request, verify } = await getSigningRequest(publicKey, t)
+
+  const tSign = t.test()
+  tSign.plan(2)
+
+  const signProcess = spawn('node', ['sign.js', request], { env })
+  signProcess.on('close', (code) => {
+    tSign.is(code, 0, '0 status code for message signing process')
+  })
+
+  let response = null
+  try {
+    signProcess.stdout.on('data', (bufferData) => {
+      const data = bufferData.toString().toLowerCase()
+      if (DEBUG_LOG) console.log('[sign]', data)
+
+      if (data.includes('confirm?')) {
+        // Enter the password
+        signProcess.stdin.write('y\n')
+      }
+
+      if (data.includes('password')) {
+        // Enter the password
+        signProcess.stdin.write(DUMMY_PASSWORD)
+      }
+
+      if (data.includes('reply with:')) {
+        response = data.split('reply with:')[1].trim()
+        tSign.pass('Successfully signed the message')
+      }
+    })
+
+    signProcess.stderr.on('data', (data) => {
+      console.error(data.toString())
+      t.fail('sign errored')
+    })
+
+    await tSign
+  } finally {
+    // To ensure the process is always killed
+    signProcess.kill('SIGKILL')
+  }
+
+  const tVerify = t.test()
+  tVerify.plan(2)
+
+  const verifyProcess = spawn('node', ['verify.js', response, request, publicKey], { env })
+  verifyProcess.on('close', (code) => {
+    tVerify.is(code, 0, '0 status code for verify process')
+  })
+
+  try {
+    let data = ''
+    verifyProcess.stdout.on('data', (bufferData) => {
+      data += bufferData.toString()
+    })
+
+    verifyProcess.stderr.on('data', (data) => {
+      console.error(data.toString())
+      t.fail('verify errored')
+    })
+
+    verifyProcess.stdout.on('close', () => {
+      if (DEBUG_LOG) console.log('[verify]', data)
+
+      if (data.includes('Signature verified.')) {
+        if (data.includes(publicKey)) {
+          tVerify.pass('Verified that the message got signed by the correct public key')
+        } else {
+          tVerify.fail('Message was signed by an incorrect public key--bug in test setup')
+        }
+      }
+    })
+
+    await tVerify
+
+    // verify against actual core
+    const { signatures } = decodeResponse(z32.decode(response))
+    t.ok(verify(signatures[0]))
+
+    // sanity check
+    signatures[0].fill(0)
+    t.absent(verify(signatures[0]))
+  } finally {
+    // To ensure the process is always killed
+    verifyProcess.kill('SIGKILL')
+  }
+})
+
+test('e2e - sign a drive', async (t) => {
+  const keysDir = await t.tmp()
+
+  const tCreateKeys = t.test()
+  tCreateKeys.plan(2)
+
+  const env = {
+    ...process.env,
+    HYPERCORE_SIGN_KEYS_DIRECTORY: keysDir
+  }
+
+  const genKeysProcess = spawn('node', ['generate-keys.js'], { env })
+  genKeysProcess.on('close', (code) => {
+    tCreateKeys.is(code, 0, 'Successfully created keys')
+  })
+
+  let publicKey = null
+  try {
+    genKeysProcess.stdout.on('data', (bufferData) => {
+      const data = bufferData.toString().toLowerCase()
+
+      if (DEBUG_LOG) console.log('[generate-keys]', data.toString())
+
+      if (data.includes('password:')) {
+        // Enter the password
+        genKeysProcess.stdin.write(DUMMY_PASSWORD)
+      }
+      if (data.includes('public key is')) {
+        tCreateKeys.pass('Key creation done')
+        publicKey = data.split('public key is ')[1].trim()
+      }
+    })
+
+    genKeysProcess.stderr.on('data', (data) => {
+      console.error(data.toString())
+      t.fail('generate-keys errored')
+    })
+
+    await tCreateKeys
+  } finally {
+    // To ensure the process is always killed
+    genKeysProcess.kill('SIGKILL')
+  }
+
+  const readPublicKey = await fs.readFile(path.join(keysDir, 'default.public'), 'utf-8')
+  t.alike(publicKey, readPublicKey, 'Public key got written to file')
+
+  const { request, verify } = await getDriveSigningRequest(publicKey, t)
+
+  const tSign = t.test()
+  tSign.plan(2)
+
+  const signProcess = spawn('node', ['sign.js', request], { env })
+  signProcess.on('close', (code) => {
+    tSign.is(code, 0, '0 status code for message signing process')
+  })
+
+  let response = null
+  try {
+    signProcess.stdout.on('data', (bufferData) => {
+      const data = bufferData.toString().toLowerCase()
+      if (DEBUG_LOG) console.log('[sign]', data)
+
+      if (data.includes('confirm?')) {
+        // Enter the password
+        signProcess.stdin.write('y\n')
+      }
+
+      if (data.includes('password')) {
+        // Enter the password
+        signProcess.stdin.write(DUMMY_PASSWORD)
+      }
+
+      if (data.includes('reply with:')) {
+        response = data.split('reply with:')[1].trim()
+        tSign.pass('Successfully signed the message')
+      }
+    })
+
+    signProcess.stderr.on('data', (data) => {
+      console.error(data.toString())
+      t.fail('sign errored')
+    })
+
+    await tSign
+  } finally {
+    // To ensure the process is always killed
+    signProcess.kill('SIGKILL')
+  }
+
+  const tVerify = t.test()
+  tVerify.plan(2)
+
+  const verifyProcess = spawn('node', ['verify.js', response, request, publicKey], { env })
+  verifyProcess.on('close', (code) => {
+    tVerify.is(code, 0, '0 status code for verify process')
+  })
+
+  try {
+    let data = ''
+    verifyProcess.stdout.on('data', (bufferData) => {
+      data += bufferData.toString()
+    })
+
+    verifyProcess.stderr.on('data', (data) => {
+      console.error(data.toString())
+      t.fail('verify errored')
+    })
+
+    verifyProcess.stdout.on('close', () => {
+      if (DEBUG_LOG) console.log('[verify]', data)
+
+      if (data.includes('Signature verified.')) {
+        if (data.includes(publicKey)) {
+          tVerify.pass('Verified that the message got signed by the correct public key')
+        } else {
+          tVerify.fail('Message was signed by an incorrect public key--bug in test setup')
+        }
+      }
+    })
+
+    await tVerify
+
+    // verify against actual core
+    const { signatures } = decodeResponse(z32.decode(response))
+    t.ok(verify(signatures))
+
+    // sanity check
+    signatures[0].fill(0)
+    t.absent(verify(signatures))
+  } finally {
+    // To ensure the process is always killed
+    verifyProcess.kill('SIGKILL')
+  }
+})
+
+test('e2e - v1 fixture', async (t) => {
+  t.plan(3)
+
+  const request = await fs.readFile(
+    path.join(__dirname, 'fixtures', 'requests', 'v1.request'),
+    'utf8'
+  )
+  const env = {
+    ...process.env,
+    HYPERCORE_SIGN_KEYS_DIRECTORY: path.join(__dirname, 'fixtures', 'keys')
+  }
+
+  const proc = spawn('node', ['sign.js', request], { env })
+
+  t.teardown(() => proc.kill('SIGKILL'))
+
+  proc.on('close', (code) => {
+    t.is(code, 0, '0 status code for message signing process')
+  })
+
+  proc.stdout.on('data', (bufferData) => {
+    const data = bufferData.toString().toLowerCase()
+    if (DEBUG_LOG) console.log('[sign]', data)
+
+    if (data.includes('confirm?')) {
+      // Enter the password
+      proc.stdin.write('y\n')
+    }
+
+    if (data.includes('password')) {
+      // Enter the password
+      proc.stdin.write('password')
+    }
+
+    if (data.includes('reply with:')) {
+      t.pass('Successfully signed the message')
+    }
+
+    if (data.includes('hypercore signing request')) {
+      t.pass()
+    } else if (data.includes('hyperdrive signing request')) {
+      t.fail()
+    }
+  })
+
+  proc.stderr.on('data', (data) => {
+    console.error(data.toString())
+    t.fail('sign errored')
+  })
+})
+
+test('e2e - v2 fixture', async (t) => {
+  t.plan(3)
+
+  const request = await fs.readFile(
+    path.join(__dirname, 'fixtures', 'requests', 'v2.request'),
+    'utf8'
+  )
+  const env = {
+    ...process.env,
+    HYPERCORE_SIGN_KEYS_DIRECTORY: path.join(__dirname, 'fixtures', 'keys')
+  }
+
+  const proc = spawn('node', ['sign.js', request], { env })
+
+  t.teardown(() => proc.kill('SIGKILL'))
+
+  proc.on('close', (code) => {
+    t.is(code, 0, '0 status code for message signing process')
+  })
+
+  proc.stdout.on('data', (bufferData) => {
+    const data = bufferData.toString().toLowerCase()
+    if (DEBUG_LOG) console.log('[sign]', data)
+
+    if (data.includes('confirm?')) {
+      // Enter the password
+      proc.stdin.write('y\n')
+    }
+
+    if (data.includes('password')) {
+      // Enter the password
+      proc.stdin.write('password')
+    }
+
+    if (data.includes('reply with:')) {
+      t.pass('Successfully signed the message')
+    }
+
+    if (data.includes('hypercore signing request')) {
+      t.pass()
+    } else if (data.includes('hyperdrive signing request')) {
+      t.fail()
+    }
+  })
+
+  proc.stderr.on('data', (data) => {
+    console.error(data.toString())
+    t.fail('sign errored')
+  })
+})
+
+test('e2e - v2 drive fixture', async (t) => {
+  t.plan(3)
+
+  const request = await fs.readFile(
+    path.join(__dirname, 'fixtures', 'requests', 'v2-drive.request'),
+    'utf8'
+  )
+  const env = {
+    ...process.env,
+    HYPERCORE_SIGN_KEYS_DIRECTORY: path.join(__dirname, 'fixtures', 'keys')
+  }
+
+  const proc = spawn('node', ['sign.js', request], { env })
+
+  t.teardown(() => proc.kill('SIGKILL'))
+
+  proc.on('close', (code) => {
+    t.is(code, 0, '0 status code for message signing process')
+  })
+
+  proc.stdout.on('data', (bufferData) => {
+    const data = bufferData.toString().toLowerCase()
+    if (DEBUG_LOG) console.log('[sign]', data)
+
+    if (data.includes('confirm?')) {
+      // Enter the password
+      proc.stdin.write('y\n')
+    }
+
+    if (data.includes('password')) {
+      // Enter the password
+      proc.stdin.write('password')
+    }
+
+    if (data.includes('reply with:')) {
+      t.pass('Successfully signed the message')
+    }
+
+    if (data.includes('hyperdrive signing request')) {
+      t.pass()
+    } else if (data.includes('hypercore signing request')) {
+      t.fail()
+    }
+  })
+
+  proc.stderr.on('data', (data) => {
+    console.error(data.toString())
+    t.fail('sign errored')
+  })
+})
+
+test('e2e - migrate legacy keys', async (t) => {
+  t.plan(4)
+
+  const dir = await tmpDir(t)
+
+  const request = await fs.readFile(
+    path.join(__dirname, 'fixtures', 'requests', 'v2-drive.request'),
+    'utf8'
+  )
+
+  await fs.cp(
+    path.join(__dirname, 'fixtures', 'keys', 'default.v0'),
+    path.join(dir, 'keys', 'default')
+  )
+  await fs.cp(
+    path.join(__dirname, 'fixtures', 'keys', 'default.public'),
+    path.join(dir, 'keys', 'default.public')
+  )
+
+  const env = {
+    ...process.env,
+    HYPERCORE_SIGN_KEYS_DIRECTORY: path.join(dir, 'keys')
+  }
+
+  const legacyKey = await fs.readFile(path.join(dir, 'keys', 'default'), 'utf8')
+  const legacyInfo = getKeyInfo(z32.decode(legacyKey))
+
+  t.is(legacyInfo.version, 0)
+
+  const proc = spawn('node', ['sign.js', request], { env })
+
+  t.teardown(() => proc.kill('SIGKILL'))
+
+  let ondone = null
+  const done = new Promise((resolve) => (ondone = resolve))
+
+  proc.on('close', (code) => {
+    t.is(code, 0, '0 status code for message signing process')
+    ondone()
+  })
+
+  proc.stdout.on('data', (bufferData) => {
+    const data = bufferData.toString().toLowerCase()
+    if (DEBUG_LOG) console.log('[sign]', data)
+
+    if (data.includes('upgrade')) {
+      t.pass()
+      // Enter the password
+      proc.stdin.write('y\n')
+    }
+
+    if (data.includes('confirm?')) {
+      // Enter the password
+      proc.stdin.write('y\n')
+    }
+
+    if (data.includes('password')) {
+      // Enter the password
+      proc.stdin.write('password')
+    }
+
+    if (data.includes('reply with:')) {
+      t.fail()
+    }
+  })
+
+  proc.stderr.on('data', (data) => {
+    console.error(data.toString())
+    t.fail('sign errored')
+  })
+
+  await done
+
+  const key = await fs.readFile(path.join(dir, 'keys', 'default'), 'utf8')
+  const info = getKeyInfo(z32.decode(key))
+
+  t.is(info.version, 1)
+})
+
+async function getSigningRequest(z32publicKey, t) {
+  const namespace = b4a.alloc(32, 1)
+  const publicKey = z32.decode(z32publicKey)
+
+  const src = new Hypercore(await t.tmp())
+
+  const core = new Hypercore(await t.tmp(), {
+    compat: false,
+    manifest: {
+      version: 1,
+      quorum: 1,
+      signers: [{ publicKey, namespace }]
+    }
+  })
+
+  t.teardown(async () => {
+    await core.close()
+    await src.close()
+  })
+
+  await core.ready()
+  await src.ready()
+
+  await src.append('Block 0')
+  await src.append('Block 1')
+
+  const request = await generate(src, { manifest: core.manifest })
+
+  return {
+    request: z32.encode(request),
+    verify(signature) {
+      const b = src.state.createTreeBatch()
+      return crypto.verify(b.signable(core.key), signature, publicKey)
+    }
+  }
+}
+
+async function getDriveSigningRequest(z32publicKey, t) {
+  const publicKey = z32.decode(z32publicKey)
+
+  const store = new Corestore(await t.tmp(), {
+    manifestVersion: 1,
+    compat: false
+  })
+  const drive = new Hyperdrive(store)
+
+  t.teardown(async () => {
+    await drive.close()
+    await store.close()
+  })
+
+  await drive.ready()
+
+  await drive.put('a', b4a.from('Block 0'))
+  await drive.put('b', b4a.from('Block 1'))
+
+  const metadataSigners = drive.core.manifest.signers.slice()
+  metadataSigners[0].publicKey = publicKey
+
+  const metadataManifest = { ...drive.core.manifest, signers: metadataSigners }
+
+  const metadataKey = Hypercore.key(metadataManifest)
+  const contentKey = Hyperdrive.getContentKey(metadataManifest)
+
+  const request = await generate(drive, { manifest: metadataManifest })
+
+  return {
+    request: z32.encode(request),
+    verify([metadata, content]) {
+      const b1 = drive.core.state.createTreeBatch()
+      const b2 = drive.blobs.core.state.createTreeBatch()
+
+      return (
+        crypto.verify(b1.signable(metadataKey), metadata, publicKey) &&
+        crypto.verify(b2.signable(contentKey), content, publicKey)
+      )
+    }
+  }
+}
