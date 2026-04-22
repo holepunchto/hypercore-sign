d971baced0787f6c950149dadc02c78019eb61b7
Marco <maidh91@gmail.com>
2026-04-22 22:30:43 +1000

    fix-flaky-test

--- test/e2e.js
+++ test/e2e.js
@@ -722,6 +722,6 @@ async function getDriveSigningRequest(z32publicKey, t) {
 
 function sliceData(data, text) {
   const sliced = data.slice(data.indexOf(text) + text.length)
-  console.log('🚀 ~ sliceData ~ slice:', sliced)
+  console.log('<slice-start>', sliced, '<slice-end>')
   return sliced
 }
